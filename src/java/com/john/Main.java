0cb3252a10c2b701534ece04dc91fa67873ff3f8
Henry Seifert and John Zhang <pair+henry+john@allstate.com>
2016-08-10 14:51:57 -0500

    main

---
+++ src/java/com/john/Main.java
@@ -0,0 +1,11 @@
+package com.john;
+
+/**
+ * Created by localadmin on 8/10/16.
+ */
+public class Main {
+
+    public static void main (String[] args) {
+        System.out.println("Welcome to Paws !!");
+    }
+}
